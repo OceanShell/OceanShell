bba0b24e38c2c00f245f3d7517e738072c5742f4
Alexander <55285809+alex-1003@users.noreply.github.com>
2019-10-20 15:01:45 +0200

    update GLODAP

--- src/osload_glodap_2019_v2_product.pas
+++ src/osload_glodap_2019_v2_product.pas
@@ -1,3 +1,24 @@
+//GLODAPv2.2019 product download
+//
+// I. First file scroll btnDataSource
+//    determining the number of variables in a file (102)
+//    number of lines in file (1175007)
+//    creation of an array of names on the first line (var_name[1..200])
+//
+// II.Second file scroll btnDownloadMD
+//    extract metadata from strings
+//    string conversion to values and date
+//    counting the number of cruises (840), stations (52299), castes (42112)
+//    and real stations (RSt#=86705)
+//    creation of an array of station boundaries in a file (by line numbers)
+//    line_err[1..100000]
+//    writing metadata to the STATION table
+//
+//III.Third file scroll btnDownloadData
+//    writing profiles into variables table
+//
+
+
 unit osload_GLODAP_2019_v2_product;
 
 {$mode objfpc}{$H+}
@@ -13,13 +34,15 @@ type
   { TfrmloadGLODAP_2019_v2_product }
 
   TfrmloadGLODAP_2019_v2_product = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
+    btnDataSource: TButton;
+    btnDownloadMD: TButton;
     btnCreateTables: TButton;
+    btnDownloadData: TButton;
     Memo1: TMemo;
     procedure btnCreateTablesClick(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure btnDataSourceClick(Sender: TObject);
+    procedure btnDownloadDataClick(Sender: TObject);
+    procedure btnDownloadMDClick(Sender: TObject);
   private
     function DateEncode(Year,Month,Day,Hour,Minutes:word;
       Var DaysInAMonthFlag,DateChangedFlag:Boolean):TDateTime;
@@ -29,9 +52,10 @@ type
 
 var
   frmloadGLODAP_2019_v2_product: TfrmloadGLODAP_2019_v2_product;
-  var_num,line_num:integer;
+  var_num,line_num,RSt:integer;
   Path, path_out:string;
-  var_name:array[1..200] of string;
+  var_name:array[1..200] of string;     //variables names
+  line_arr:array[1..100000] of integer; //profile intervals at stations
   Dat, out:text;
 
 implementation
@@ -44,11 +68,12 @@ uses osmain, procedures, dm;
 
 
 
-procedure TfrmloadGLODAP_2019_v2_product.Button1Click(Sender: TObject);
+procedure TfrmloadGLODAP_2019_v2_product.btnDataSourceClick(Sender: TObject);
 var
   k,n,line:integer;
   symbol:char;
   st,buf_str:string;
+
 begin
   path:='c:\Users\ako071\AK\datasets\GLODAP\GLODAPv2.2019_Merged_Master_File.csv';
   //path:='c:\Users\ako071\AK\datasets\GLODAP\cruise 1116a.csv';
@@ -57,19 +82,20 @@ begin
 
   AssignFile(dat, Path); Reset(dat);
   readln(dat, st);
+  st:=trim(st);
 
   //number of variables in the first line
-  if trim(st)<>'' then begin
     var_num:=0;
   for k:=1 to length(st) do begin
     symbol:=st[k];
     if symbol=',' then var_num:=var_num+1;
   end;
-  end;
     var_num:=var_num+1;
-    memo1.Lines.Add('var_num='+inttostr(var_num));
+    memo1.Lines.Add('Variables in file:'+inttostr(var_num));
 
-  //create array with variable names from the first line
+
+  //array with variable names
+  for k:=1 to 200 do var_name[k]:='';
     n:=0;
   for k:=1 to var_num do begin
     buf_str:='';
@@ -80,6 +106,8 @@ begin
   until (symbol=',') or (symbol=#10);
     var_name[k]:=buf_str;
   end;
+  memo1.Lines.Add('Array with variable names is created');
+
 
   //count lines
   line:=1;
@@ -92,14 +120,13 @@ begin
     closefile(dat);
   end;
     line_num:=line;
-    memo1.Lines.Add('line#='+inttostr(line));
+    memo1.Lines.Add('Lines in file    :'+inttostr(line));
 
-  //CloseFile(dat);
-  Button2.Visible:=true;
+  btnDownloadMD.Visible:=true;
 end;
 
 
-procedure TfrmloadGLODAP_2019_v2_product.Button2Click(Sender: TObject);
+procedure TfrmloadGLODAP_2019_v2_product.btnDownloadMDClick(Sender: TObject);
 var
 k,kv,line,n,mik:integer;
 cruiseN,stationN,castN,stNBNum:integer;
@@ -112,30 +139,56 @@ StDate,StTime,StDT:TDateTime;
 DayChange,DateChange:Boolean;
 ww:boolean; //workedwell
 
-press,temp,salt,oxy,aou,nat,nit,sil,pho,tco2,talk,phts25p0,phtsinsitutp,cfcll:real;
-salt_pQF1,oxy_pQF1,aou_pQF1,nat_pQF1,nit_pQF1,sil_pQF1,pho_pQF1,tco2_pQF1,talk_pQF1:integer;
-phtsinsitutp_pQF1, phts25p0_pQF1, cfcll_pQF1: integer;
-salt_sQF,oxy_sQF,nat_sQF,sil_sQF,pho_sQF,tco2_sQF,talk_sQF,phtsinsitutp_sQF, phts25p0_sQF:integer;
 
-//cfcll:real;
+//values
+press,temp,salt,oxy,aou,nat,nit,sil,pho,tco2,talk,phts25p0,phtsinsitutp:real;
+cfc11,pcfc11,cfc12,pcfc12,cfc113,pcfc113,cc14,pcc14,sf6,psf6,c13,c14:real;
+h3,he3,he,neon,O18,toc,doc,don,tdn,chla:real;
+//errors
+c14_err,h3_err,he3_err,he_err,neon_err:real;
+//QF primary 1
+salt_pQF1,oxy_pQF1,aou_pQF1,nat_pQF1,nit_pQF1,sil_pQF1,pho_pQF1:integer;
+tco2_pQF1,talk_pQF1,phtsinsitutp_pQF1,cfc11_pQF1,cfc12_pQF1,cfc113_pQF1:integer;
+cc14_pQF1,c13_pQF1,c14_pQF1,h3_pQF1,he3_pQF1,he_pQF1,neon_pQF1:integer;
+O18_pQF1,toc_pQF1,doc_pQF1,don_pQF1,tdn_pQF1,chla_pQF1,phts25p0_pQF1:integer;
+//QF secondary
+salt_sQF,oxy_sQF,nat_sQF,sil_sQF,pho_sQF,tco2_sQF,talk_sQF,phtsinsitutp_sQF:integer;
+cfc11_sQF,cfc12_sQF,cfc113_sQF,cc14_sQF,sf6_pQF1,c13_sQF,phts25p0_sQF:integer;
+//divide file on stations/casts
+cruiseNbuf,stationNbuf,castNbuf:Integer;
+cruise_count,station_count,cast_count:Integer;
+newCruise,newStation,newCast,NewMD:Boolean;
+
+
+
 
 begin
    path_out:='c:\Users\ako071\AK\datasets\GLODAP\output.dat';
    AssignFile(out, Path_out); Rewrite(out);
+   writeln(out,'Rst#  line#  cruise#  st#  cast#  date  lat  lon  BD  LastPress bottle#');
+
 
    memo1.Lines.Add('');
-   memo1.Lines.Add('var_num ='+inttostr(var_num));
+   memo1.Lines.Add('Variables');
+   //memo1.Lines.Add('Variables in file:'+inttostr(var_num));
    for k:=1 to var_num do memo1.Lines.Add(inttostr(k)+#9+var_name[k]);
-   memo1.Lines.Add('line_num='+inttostr(line_num));
+   //memo1.Lines.Add('Lines in file:'+inttostr(line_num));
 
 
    Reset(dat);
    readln(dat, st);
    line:=1;
+
+   cruise_count:=1;
+   station_count:=1;
+   cast_count:=1;
+
+   RSt:=0; //Real Station
 //{r}repeat
 //{w}while not EOF(dat) do begin
-{f}for k:=1 to line_num-1 do begin
+{k}for k:=1 to line_num-1 do begin
      readln(dat, st);
+     st:=trim(st);
      line:=line+1;
      //showmessage('line='+inttostr(line)+'  length='+inttostr(length(st)));
 
@@ -144,9 +197,13 @@ begin
      stPDS:=-9999;
      stNBNum:=-9999;
 
+     newCruise:=false;
+     newStation:=false;
+     newCast:=false;
+
 //string analysis
      n:=0;
-{k}for kv:=1 to var_num do begin
+{kv}for kv:=1 to var_num do begin
      buf_str:='';
 {s}repeat
      inc(n);
@@ -159,13 +216,13 @@ begin
      //            +' ASCII='+inttostr(ord(symbol))+' buf='+buf_str);
 
 {s}until (symbol=',') or (n=length(st));
+
 {b}if buf_str<>'' then begin
 
   if not TryStrToFloat(buf_str, buf)
   then showmessage ('trystrtofloat line='+inttostr(line)+' '+buf_str);
 
 
-(***********************************COMMENTED out to compile the code - AS
     case kv of
     1: cruiseN:=trunc(strtofloat(buf_str));
     2: stationN:=trunc(strtofloat(buf_str));
@@ -192,8 +249,8 @@ begin
    28: oxy_pQF1:=trunc(strtofloat(buf_str));        //pQF1
    29: oxy_sQF:=trunc(strtofloat(buf_str));         //sQF
 
-   27: aou:=strtofloat(buf_str);                    //apparant oxygen utilization
-   28: aou_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   30: aou:=strtofloat(buf_str);                    //apparant oxygen utilization
+   31: aou_pQF1:=trunc(strtofloat(buf_str));        //pQF1
 
    32: nat:=strtofloat(buf_str);                    //nitrate
    33: nat_pQF1:=trunc(strtofloat(buf_str));        //pQF1
@@ -210,47 +267,100 @@ begin
    41: pho_pQF1:=trunc(strtofloat(buf_str));        //pQF1
    42: pho_sQF:=trunc(strtofloat(buf_str));         //sQF
 
-   43: tco2:=strtofloat(buf_str);                    //tco2
+   43: tco2:=strtofloat(buf_str);                    //tco2:Dissolved inorganic carbon
    44: tco2_pQF1:=trunc(strtofloat(buf_str));        //pQF1
    45: tco2_sQF:=trunc(strtofloat(buf_str));         //sQF
 
-   43: talk:=strtofloat(buf_str);                    //talk
-   44: talk_pQF1:=trunc(strtofloat(buf_str));        //pQF1
-   45: talk_sQF:=trunc(strtofloat(buf_str));         //sQF
+   46: talk:=strtofloat(buf_str);                    //talk
+   47: talk_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   48: talk_sQF:=trunc(strtofloat(buf_str));         //sQF
 
    49: phts25p0:=strtofloat(buf_str);                    //ph ts 25 p0
    50: phts25p0_pQF1:=trunc(strtofloat(buf_str));        //pQF1
-   53: phts25p0_sQF:=trunc(strtofloat(buf_str));         //sQF
+   53: begin
+        phts25p0_sQF:=trunc(strtofloat(buf_str));         //sQF
+        phtsinsitutp_sQF:=trunc(strtofloat(buf_str));         //sQF
+       end;
 
    51: phtsinsitutp:=strtofloat(buf_str);                    //ph ts insitu tp
    52: phtsinsitutp_pQF1:=trunc(strtofloat(buf_str));        //pQF1
-   53: phtsinsitutp_sQF:=trunc(strtofloat(buf_str));         //sQF
 
-   51: cfc11:=strtofloat(buf_str);                    //cfc11
-   51: pcfc11:=strtofloat(buf_str);                   //cfc11
-   52: cfc11_pQF1:=trunc(strtofloat(buf_str));        //pQF1
-   53: cfc11_sQF:=trunc(strtofloat(buf_str));         //sQF
- }
+   54: cfc11:=strtofloat(buf_str);                    //cfc11:transient tracer
+   55: pcfc11:=strtofloat(buf_str);                   //pcfc11
+   56: cfc11_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   57: cfc11_sQF:=trunc(strtofloat(buf_str));         //sQF
 
-{
-press,temp,salt,oxy,aou,nat,nit,sil,pho,tco2,talk,phts25p0,phtsinsitutp
-salt_pQF1,oxy_pQF1,aou_pQF1,nat_pQF1,nit_pQF1,sil_pQF1,pho_pQF1,tco2_pQF1,talk_pQF1
-phtsinsitutp_pQF1
-salt_sQF,oxy_sQF,nat_sQF,sil_sQF,pho_sQF,tco2_sQF,talk_sQF,phtsinsitutp_sQF}
+   58: cfc12:=strtofloat(buf_str);                    //cfc12:transient tracer
+   59: pcfc12:=strtofloat(buf_str);                   //pcfc12
+   60: cfc12_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   61: cfc12_sQF:=trunc(strtofloat(buf_str));         //sQF
 
+   62: cfc113:=strtofloat(buf_str);                    //cfc113:transient tracer
+   63: pcfc113:=strtofloat(buf_str);                   //pcfc113
+   64: cfc113_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   65: cfc113_sQF:=trunc(strtofloat(buf_str));         //sQF
+
+   66: cc14:=strtofloat(buf_str);                    //cc14:transient tracer
+   67: pcc14:=strtofloat(buf_str);                   //pcc14
+   68: cc14_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   69: cc14_sQF:=trunc(strtofloat(buf_str));         //sQF
+
+   70: sf6:=strtofloat(buf_str);                    //sf6:Sulfur hexafluoride
+   71: psf6:=strtofloat(buf_str);                   //psf6
+   72: sf6_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+   73: c13:=strtofloat(buf_str);                    //c13 Stable isotop carbon 13
+   74: c13_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+   75: c13_sQF:=trunc(strtofloat(buf_str));         //sQF
+
+   76: c14:=strtofloat(buf_str);                   //c14 Radioisotop carbon 14
+   77: c14_pQF1:=trunc(strtofloat(buf_str));       //pQF1
+   78: c14_err:=strtofloat(buf_str);               //counting error
+
+   79: h3:=strtofloat(buf_str);                   //h3:radioisotop hydrogen 3, Tritium
+   80: h3_pQF1:=trunc(strtofloat(buf_str));       //pQF1
+   81: h3_err:=strtofloat(buf_str);               //counting error
+
+   82: he3:=strtofloat(buf_str);                   //he3:Radioisotop helium 3
+   83: he3_pQF1:=trunc(strtofloat(buf_str));       //pQF1
+   84: he3_err:=strtofloat(buf_str);               //counting error
+
+   85: he:=strtofloat(buf_str);                    //he:helium
+   86: he_pQF1:=trunc(strtofloat(buf_str));       //pQF1
+   87: he_err:=strtofloat(buf_str);               //counting error
+
+   88: neon:=strtofloat(buf_str);                    //neon
+   89: neon_pQF1:=trunc(strtofloat(buf_str));       //pQF1
+   90: neon_err:=strtofloat(buf_str);               //counting error
+
+   91: O18:=strtofloat(buf_str);                    //O18:Stable isotop of oxygen 18
+   92: O18_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+   93: toc:=strtofloat(buf_str);                    //toc:Total organic carbon
+   94: toc_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+   95: doc:=strtofloat(buf_str);                    //doc:Dissolved organic carbon
+   96: doc_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+   97: don:=strtofloat(buf_str);                    //don:Dissolved organic nitrogen
+   98: don_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+   99: tdn:=strtofloat(buf_str);                    //tdn:Total dissolved nitrogen
+  100: tdn_pQF1:=trunc(strtofloat(buf_str));        //pQF1
+
+  101: chla:=strtofloat(buf_str);                    //chla:chlorophylla
+  102: chla_pQF1:=trunc(strtofloat(buf_str));        //pQF1
     end;{case}
-*)  //END of commenting out - AS
 
-{k}end;
+{kv}end;
 
 {b}end;
 
+    //dates
     //stDate:=EncodeDate(year,month,day);
     //stTime:=EncodeTime(hour,min,0,0);
     //ODBPr_ConvertDateTime(year,month,day,hour,min,StDate,StTime,MonthErr,TimeErr);
-
     StDT:= DateEncode(Year,Month,Day,Hour,Min,DayChange,DateChange);
-
     if DayChange=true then memo1.Lines.Add('Day was changed in line='
                            +inttostr(line)
                            +'  '+inttostr(day)+'.'+inttostr(month)+'.'+inttostr(year)
@@ -260,37 +370,181 @@ salt_sQF,oxy_sQF,nat_sQF,sil_sQF,pho_sQF,tco2_sQF,talk_sQF,phtsinsitutp_sQF}
                            +'  '+inttostr(day)+'.'+inttostr(month)+'.'+inttostr(year)
                            +'  '+inttostr(hour)+':'+inttostr(min));
 
+    //count cruise, stations, casts
+    if k=1 then begin
+          cruiseNbuf:=cruiseN;
+          stationNbuf:=stationN;
+          castNbuf:=cruiseN;
+    end;
 
-     writeln(out,inttostr(line),
-     #9,inttostr(cruiseN),
-     #9,inttostr(stationN),
-     #9,inttostr(castN),
-     //#9,datetostr(stdate),
-     //#9,timetostr(sttime),
-     #9,datetimetostr(stDT),
-     #9,floattostr(stlat),
-     #9,floattostr(stlon),
-     #9,floattostr(stBD),
-     #9,floattostr(stPDS),
-     #9,inttostr(stNBNum)
-     );
+    if k>1 then begin
+      if cruiseN<>cruiseNbuf then begin newCruise:=true; cruise_count:=cruise_count+1; end;
+      if stationN<>stationNbuf then begin newStation:=true; station_count:=station_count+1; end;
+      if castN<>castNbuf then begin newCast:=true; cast_count:=cast_count+1; end;
+
+      newMD:=false;
+      if(newCruise=true)  then newMD:=true;
+      if(newStation=true) then newMD:=true;
+      if(newStation=false) and (newCast=true) then newMD:=true;
+      //if(newCruise=false) and (newStation=true)  and (newCast=true) then newMD:=true;
+      //if(newCruise=false) and (newStation=false) and (newCast=true) then newMD:=true;
+    end;
+
+
+{MD}if newMD=true then begin
+      RSt:=RSt+1;
+      line_arr[RSt]:=line;
+
+      cruiseNbuf:=cruiseN;
+      stationNbuf:=stationN;
+      castNbuf:=castN;
+
+
+      writeln(out,inttostr(RSt),
+      #9,inttostr(line),
+      #9,inttostr(cruiseN),
+      #9,inttostr(stationN),
+      #9,inttostr(castN),
+      //#9,datetostr(stdate),
+      //#9,timetostr(sttime),
+      #9,datetimetostr(stDT),
+      #9,floattostr(stlat),
+      #9,floattostr(stlon),
+      #9,floattostr(stBD),
+      #9,floattostr(stPDS),
+      #9,inttostr(stNBNum)
+      );
 
     //memo1.Lines.Add(inttostr(line)
     //+#9+inttostr(cruiseN)
     //+#9+inttostr(stationN)
     //+#9+inttostr(castN)
     //);
+{MD}end;
+
 
 //{r}until eof(dat);
 //{w}end;
-{f}end;
+{k}end; {lines }
 
    CloseFile(dat);
    CloseFile(out);
 
    memo1.Lines.Add('End of file');
+   memo1.Lines.Add('cruises#       ='+inttostr(cruise_count));
+   memo1.Lines.Add('stations#      ='+inttostr(station_count));
+   memo1.Lines.Add('casts#         ='+inttostr(cast_count));
+   memo1.Lines.Add('Real Stations# ='+inttostr(RSt));
+
+   btnDownloadData.Visible:=true;
+end;
+
+
+
+procedure TfrmloadGLODAP_2019_v2_product.btnDownloadDataClick(Sender: TObject);
+var
+  kst,kl,kv,line,L1,L2,n:integer;
+  cruiseN,stationN,castN,stNBNum:integer;
+  Year,Month,Day,Hour,Min:integer;
+  press, temp,stlat,stlon,stBD,stPDS:real;
+  symbol:char;
+  st,buf_str:string;
+  StDT:TDateTime;
+  DayChange,DateChange:Boolean;
+
+begin
+   path_out:='c:\Users\ako071\AK\datasets\GLODAP\output_profiles.dat';
+   AssignFile(out, Path_out); Rewrite(out);
+   writeln(out,'RSt#  L1  L2  line#  cruise  station  cast  bottle#  press  temp');
+
+
+   Reset(dat);
+   readln(dat, st);
+   line:=1;
+
+
+//first prepare data for one table P_TEMPERATURE_BOTTLE
+{st}for kst:=1 to RSt do begin
+      L1:=line_arr[kst];
+      L2:=line_arr[kst+1]-1;
+
+
+{L}for kl:=L1 to L2 do begin    //file scroll
+      readln(dat, st);
+      st:=trim(st);
+      line:=line+1;
+
+{pr}if (line>=L1) and (line<=L2) then begin  //inside profile
+
+//string analysis
+       n:=0;
+  {kv}for kv:=1 to var_num do begin
+       buf_str:='';
+  {s}repeat
+       inc(n);
+       symbol:=st[n];
+       if (symbol<>',') then buf_str:=buf_str+symbol;
+  {s}until (symbol=',') or (n=length(st));
+
+  {b}if buf_str<>'' then begin
+      case kv of
+      1: cruiseN:=trunc(strtofloat(buf_str));
+      2: stationN:=trunc(strtofloat(buf_str));
+      3: castN:=trunc(strtofloat(buf_str));
+      4: year:=trunc(strtofloat(buf_str));
+      5: month:=trunc(strtofloat(buf_str));
+      6: day:=trunc(strtofloat(buf_str));
+      7: hour:=trunc(strtofloat(buf_str));
+      8: min:=trunc(strtofloat(buf_str));
+      9: stlat:=strtofloat(buf_str);
+     10: stlon:=strtofloat(buf_str);
+     11: stBD:=strtofloat(buf_str);                    //bottom depth m
+     12: stPDS:=strtofloat(buf_str);                   //pressure of the deepest sample
+     13: stNBNum:=trunc(strtofloat(buf_str));          //Niskin bottle number
+
+     14: press:=strtofloat(buf_str);                   //sampling pressure dbar
+     16: temp:=strtofloat(buf_str);                    //temperature
+      end;{case}
+  {b}end;
+
+  {kv}end;
+//end string analysis
+
+     writeln(out,inttostr(kst),
+     #9+inttostr(L1),
+     #9+inttostr(L2),
+     #9,inttostr(line),
+     #9,inttostr(cruiseN),
+     #9,inttostr(stationN),
+     #9,inttostr(castN),
+     #9,inttostr(stNBNum),
+     #9,floattostr(press),
+     #9,floattostr(temp));
+
+
+{pr}end;{inside profiles}
+{L} end; {lines loop}
+
+    StDT:= DateEncode(Year,Month,Day,Hour,Min,DayChange,DateChange);
+    writeln(out,'...new Real Station'
+    +'  date: '+datetimetostr(StDT)
+    +'  lat: '+floattostr(stlat)
+    +'  lon: '+floattostr(stlon)
+    +'  BD: '+floattostr(stBD)
+    +'  PressOfDeepestSample: '+floattostr(stPDS)
+    );
+
+{st}end; //real stations loop
+    closefile(out);
+
+    memo1.Lines.Add('');
+    memo1.Lines.Add('Loading completed');
+
 
 end;
+
+
+
 
 function TfrmloadGLODAP_2019_v2_product.DateEncode(Year,Month,Day,Hour,Minutes:word;
   Var DaysInAMonthFlag,DateChangedFlag:Boolean):TDateTime;
@@ -354,8 +608,6 @@ begin
   end;
 
 end;
-
-
 
 procedure TfrmloadGLODAP_2019_v2_product.btnCreateTablesClick(Sender: TObject);
 Var
@@ -850,6 +1102,8 @@ begin
   TR.Free;
  end;
 end;
+
+
 
 end.
 
