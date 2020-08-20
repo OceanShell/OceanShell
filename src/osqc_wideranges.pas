d2dcc2bad038cd9dad5c45c546642f32b14f8157
Alexander <55285809+alex-1003@users.noreply.github.com>
2020-08-20 17:40:39 +0200

    update
    wide ranges

--- src/osqc_wideranges.pas
+++ src/osqc_wideranges.pas
@@ -22,6 +22,9 @@ type
     Edit3: TEdit;
     Edit4: TEdit;
     Edit5: TEdit;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    GroupBox1: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
@@ -29,6 +32,8 @@ type
     Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     Memo1: TMemo;
     RadioGroup1: TRadioGroup;
     StringGrid1: TStringGrid;
@@ -59,6 +64,8 @@ var
 ktbl :integer;
 tbl,var_name :string;
 begin
+
+  memo1.Clear;
 
   CheckGroup1.items:=frmosmain.ListBox1.Items;
 
@@ -190,7 +197,7 @@ range_unknown,isconverted,isoutlier :boolean;
 DT1,DT2: TDateTime;
 
 {PQF2 update}
-id,instr_id,prof_num: integer;
+id,instr_id,prof_num,PQF2,PQF2_count: integer;
 lev_m :real;
 
 {lab density}
@@ -211,15 +218,22 @@ begin
 {T}for ktbl:=0 to CheckGroup1.Items.Count-1 do begin
 {C}if CheckGroup1.Checked[ktbl] then begin
 
+   Edit1.Text:='';
+   Edit2.Text:='';
+   Edit3.Text:='';
+   Edit4.Text:='';
+   Edit5.Text:='';
+   Edit6.Text:='';
+
    tbl:=CheckGroup1.Items.Strings[ktbl]; {selected table}
    Edit1.Text:=tbl;
+   Application.ProcessMessages;
 
    {...apply range}
 {R}for i:=1 to StringGrid1.RowCount-1 do begin
     var_name:=StringGrid1.Cells[0,i];
 {V}if tbl='P_'+var_name then begin
 
-    memo1.Lines.Add(tbl);
     range_unknown:=false;
 
     with frmdm.q1 do begin
@@ -241,6 +255,8 @@ begin
     end;
 
 {WF}if CheckBox1.Checked = false then begin
+
+   memo1.Lines.Add(tbl+'   ...control run');
 
    sq_count:=0;
    val_t:=0;    {number of               values in a table}
@@ -272,7 +288,9 @@ begin
       SQL.Add(' select val,units_id from STATION,'+tbl);
       SQL.Add(' where station.id='+tbl+'.id ');
       SQL.Add(' and latitude>:lts and latitude<=:ltn ');
-      SQL.Add(' and longitude>=:lnw and longitude<:lne ');
+      {...select data from 180 meridian}
+      if lne<>180 then SQL.Add(' and longitude>=:lnw and longitude<:lne ')
+                  else SQL.Add(' and longitude>=:lnw and longitude<=:lne ');
       ParamByName('ltn').AsFloat:=ltn;
       ParamByName('lts').AsFloat:=lts;
       ParamByName('lnw').AsFloat:=lnw;
@@ -322,6 +340,7 @@ begin
     if isconverted=true then begin
       val:=val_conv;
       val_c:=val_c+1;
+      Edit6.Text:=inttostr(val_c);
       isoutlier:=false;
       if val<val_min then isoutlier:=true;
       if val>val_max then isoutlier:=true;
@@ -330,8 +349,10 @@ begin
         Edit5.Text:=inttostr(outliers_count);
       end;
     end
-    else val_nc:=val_nc+1;
-
+    else begin
+       val_nc:=val_nc+1;
+       Edit7.Text:=inttostr(val_nc);
+    end;
 {c}end;
 
      frmdm.q1.Next;
@@ -357,6 +378,26 @@ begin
 
 {......update flags in DB}
 {WT}if CheckBox1.Checked = true then begin
+
+   memo1.Lines.Add('');
+   memo1.Lines.Add(tbl+'   ...flags writing ');
+
+   memo1.Lines.Add(tbl+'   PQF2 statistics: initial');
+   with frmdm.q3 do begin
+     Close;
+     SQL.Clear;
+     SQL.Add(' select PQF2, count(PQF2) from '+tbl);
+     SQL.Add(' group by PQF2 ');
+     Open;
+   end;
+   while not frmdm.q3.EOF do begin
+     PQF2:=frmdm.q3.FieldByName('PQF2').AsInteger;
+     PQF2_count:=frmdm.q3.FieldByName('count').AsInteger;
+     memo1.Lines.Add(inttostr(PQF2)+#9+inttostr(PQF2_count));
+     frmdm.q3.Next;
+   end;
+     frmdm.q3.Close;
+
 
    sq_count:=0;
    val_t:=0;    {number of               values in a table}
@@ -385,18 +426,18 @@ begin
     with frmdm.q1 do begin
       Close;
       SQL.Clear;
-      SQL.Add(' select id,lev_m,val,units_id,instrument_id,profile_number from STATION,'+tbl);
+      SQL.Add(' select STATION.id as id,lev_m,val,units_id,instrument_id,profile_number from STATION,'+tbl);
       SQL.Add(' where station.id='+tbl+'.id ');
       SQL.Add(' and latitude>:lts and latitude<=:ltn ');
-      SQL.Add(' and longitude>=:lnw and longitude<:lne ');
+      {...select data from 180 meridian}
+      if lne<>180 then SQL.Add(' and longitude>=:lnw and longitude<:lne ')
+                  else SQL.Add(' and longitude>=:lnw and longitude<=:lne ');
       ParamByName('ltn').AsFloat:=ltn;
       ParamByName('lts').AsFloat:=lts;
       ParamByName('lnw').AsFloat:=lnw;
       ParamByName('lne').AsFloat:=lne;
       Open;
     end;
-
-
 
 {w}while not frmdm.q1.EOF do begin
      id:=frmdm.q1.FieldByName('id').AsInteger;
@@ -455,6 +496,7 @@ begin
 {conv}if isconverted=true then begin
       val:=val_conv;
       val_c:=val_c+1;
+      Edit6.Text:=inttostr(val_c);
       isoutlier:=false;
       if val<val_min then isoutlier:=true;
       if val>val_max then isoutlier:=true;
@@ -480,8 +522,10 @@ begin
 {OUT}end;
 
 {conv}end
-      else val_nc:=val_nc+1;
-
+      else begin
+         val_nc:=val_nc+1;
+         Edit7.Text:=inttostr(val_nc);
+      end;
 {c}end;
 
      frmdm.q1.Next;
@@ -492,6 +536,8 @@ begin
 {Ln}until lne=180;
 {Lt}until lts=-90;
 
+     frmdm.TR.Commit;
+
      Edit4.Text:=inttostr(val_t);
 
      memo1.Lines.Add(inttostr(i)
@@ -501,6 +547,55 @@ begin
      +#9+'unit_default='+inttostr(unit_default)
      +#9+'outliers='+inttostr(outliers_count)
      +#9+'total/converted/not_converted='+inttostr(val_t)+'/'+inttostr(val_c)+'/'+inttostr(val_nc));
+
+
+     memo1.Lines.Add('');
+     memo1.Lines.Add(tbl+'   PQF2 statistics: after flags writing ');
+     with frmdm.q3 do begin
+       Close;
+       SQL.Clear;
+       SQL.Add(' select PQF2, count(PQF2) from '+tbl);
+       SQL.Add(' group by PQF2 ');
+       Open;
+     end;
+     while not frmdm.q3.EOF do begin
+       PQF2:=frmdm.q3.FieldByName('PQF2').AsInteger;
+       PQF2_count:=frmdm.q3.FieldByName('count').AsInteger;
+       memo1.Lines.Add(inttostr(PQF2)+#9+inttostr(PQF2_count));
+       frmdm.q3.Next;
+     end;
+       frmdm.q3.Close;
+
+
+     {...change all PQF2=0 to PQF2=4 }
+     with frmdm.q3 do begin
+      Close;
+       SQL.Clear;
+       SQL.Add(' UPDATE '+tbl+' SET PQF2=4 WHERE PQF2=0 ');
+      ExecSQL;
+     end;
+       frmdm.TR.Commit;
+
+
+       memo1.Lines.Add('');
+       memo1.Lines.Add(tbl+'   PQF2 statistics: final ');
+       with frmdm.q3 do begin
+         Close;
+         SQL.Clear;
+         SQL.Add(' select PQF2, count(PQF2) from'+tbl);
+         SQL.Add(' group by PQF2 ');
+         Open;
+       end;
+       while not frmdm.q3.EOF do begin
+         PQF2:=frmdm.q3.FieldByName('PQF2').AsInteger;
+         PQF2_count:=frmdm.q3.FieldByName('count').AsInteger;
+         memo1.Lines.Add(inttostr(PQF2)+#9+inttostr(PQF2_count));
+         frmdm.q3.Next;
+       end;
+         frmdm.q3.Close;
+
+
+
 
 {WT}end; {write true - control without flags writing}
 
