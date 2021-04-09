8bd4c148503a142e28d6a526225d1bf987752f22
axline <axline@mail.ru>
2021-04-09 11:23:15 +0700

    osmain update

--- src/osmain.pas
+++ src/osmain.pas
@@ -225,7 +225,6 @@ type
     MenuItem25: TMenuItem;
     iMetadataUpdate: TMenuItem;
     iDensity: TMenuItem;
-    iLoad_ICES_AK: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
     iSelectCruise: TMenuItem;
@@ -428,7 +427,6 @@ type
     procedure iLoadITPClick(Sender: TObject);
     procedure iLoad_GLODAP_2019_v2_productClick(Sender: TObject);
     procedure iload_icesClick(Sender: TObject);
-    procedure iLoad_ICES_AKClick(Sender: TObject);
     procedure iLoad_ITPClick(Sender: TObject);
     procedure iLoad_Pangaea_CTD_tabClick(Sender: TObject);
     procedure iLoad_WOD18Click(Sender: TObject);
@@ -628,7 +626,6 @@ uses
   osload_WOD18,
   osload_PangaeaTab,
   osload_ices,
-  osload_ices1,
 
 (* database service procedures *)
   ossupporttables,
@@ -4346,17 +4343,6 @@ begin
    frmload_ices.Free;
    frmload_ices := nil;
  end;
-end;
-
-procedure Tfrmosmain.iLoad_ICES_AKClick(Sender: TObject);
-begin
-  frmload_ices1 := Tfrmload_ices1.Create(Self);
-  try
-   if not frmload_ices1.ShowModal = mrOk then exit;
-  finally
-    frmload_ices1.Free;
-    frmload_ices1 := nil;
-  end;
 end;
 
 procedure Tfrmosmain.iLoad_ITPClick(Sender: TObject);
