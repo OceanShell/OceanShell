c882881ced9904f9ca368dbda73024a86ec43f46
axline <axline@mail.ru>
2021-11-30 09:33:27 +0700

    Duplicates updated

--- src/osqc_duplicates.pas
+++ src/osqc_duplicates.pas
@@ -15,24 +15,22 @@ type
   Tfrmqc_duplicates = class(TForm)
     btnFindDuplicates: TButton;
     btnUpdateCruise: TButton;
-    Button1: TButton;
-    cbSource: TComboBox;
+    btnMarkCruiseDuplicate: TButton;
     GroupBox3: TGroupBox;
     Label5: TLabel;
-    GroupBox1: TGroupBox;
     GroupBox2: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
-    seRatio: TFloatSpinEdit;
+    seRatio: TSpinEdit;
     seThreshold: TFloatSpinEdit;
     seDateThreshold: TSpinEdit;
+
     procedure btnFindDuplicatesClick(Sender: TObject);
     procedure btnUpdateCruiseClick(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
-    procedure cbSourceChange(Sender: TObject);
-    procedure FormShow(Sender: TObject);
+    procedure btnMarkCruiseDuplicateClick(Sender: TObject);
+
   private
 
   public
@@ -50,37 +48,20 @@ implementation
 
 uses osmain, dm, procedures;
 
-procedure Tfrmqc_duplicates.FormShow(Sender: TObject);
-begin
- with frmdm.q1 do begin
-   Close;
-     SQL.Clear;
-     SQL.Add('SELECT NAME FROM SOURCE ORDER BY NAME ');
-   Open;
- end;
-
- cbSource.Clear;
- while not frmdm.q1.EOF do begin
-    cbSource.Items.Add(frmdm.q1.Fields[0].AsString);
-   frmdm.q1.Next;
- end;
-end;
-
-procedure Tfrmqc_duplicates.cbSourceChange(Sender: TObject);
-begin
-  btnFindDuplicates.Enabled:=true;
-  btnUpdateCruise.Enabled:=true;
-end;
 
 procedure Tfrmqc_duplicates.btnFindDuplicatesClick(Sender: TObject);
 Var
 TRt:TSQLTransaction;
 Qt1, Qt2, Qt3:TSQLQuery;
 
-k, cnt_dup:integer;
+k, cnt_dup, r_lev:integer;
 begin
 
+
+try
 btnFindDuplicates.Enabled:=false;
+btnUpdateCruise.Enabled:=false;
+btnMarkCruiseDuplicate.Enabled:=false;
 
 TRt:=TSQLTransaction.Create(self);
 TRt.DataBase:=frmdm.IBDB;
@@ -93,28 +74,38 @@ Qt2 :=TSQLQuery.Create(self);
 Qt2.Database:=frmdm.IBDB;
 Qt2.Transaction:=TRt;
 
- try
-   (* Select everyting except the specified source *)
+(*
+There are 3 ratings:
+1 - the highest, initial sources
+2 - trusted compilations
+3 - al the rest
+
+We start with selecting all the stations with the rating=1, and then look for
+duplicates in stations with the rating=2. Then we select all the stations with
+raitings<=2, and look for duplicates in stations with the rating =3 *)
+
+  cnt_dup:=0;
+  for r_lev:=1 to 2 do begin //rating levels
+
+   (* Select the highest rating first *)
    with Qt1 do begin
     Close;
      SQL.Clear;
      SQL.Add(' SELECT STATION.ID, LATITUDE, LONGITUDE, DATEANDTIME ');
      SQL.Add(' FROM STATION WHERE ');
-    // SQL.Add(' DUPLICATE=FALSE AND ');
-     SQL.Add(' CRUISE_ID NOT IN (SELECT CRUISE.ID FROM CRUISE, SOURCE WHERE ');
-     SQL.Add(' CRUISE.SOURCE_ID=SOURCE.ID AND SOURCE.NAME=:SrcName) ');
-   //  SQL.Add(' AND DATEANDTIME BETWEEN '+QuotedStr('01.01.1935'));
-   ///  SQL.Add(' and '+QuotedStr('31.12.1935'));
-   //  SQL.Add(' and LATITUDE>60 ');
+     SQL.Add(' CRUISE_ID IN (SELECT CRUISE.ID FROM CRUISE, SOURCE WHERE ');
+     SQL.Add(' CRUISE.SOURCE_ID=SOURCE.ID AND SOURCE.PRIORITY <= :R_LEV ');
+     SQL.Add(' AND SOURCE.NAME <> '+QuotedStr('ARGO'));
+     SQL.Add(' AND SOURCE.NAME <> '+QuotedStr('ITP')+')');
      SQL.Add(' ORDER BY STATION.ID ');
-     ParamByName('SrcName').AsString:=cbSource.Text;
+     ParamByName('R_LEV').Value:=r_lev;
     Open;
     Last;
     First;
    end;
 
 
-   k:=0; cnt_dup:=0;
+   k:=0;
    while not Qt1.EOF do begin
     inc(k);
      with Qt2 do begin
@@ -124,12 +115,12 @@ Qt2.Transaction:=TRt;
        SQL.Add(' FROM STATION, CRUISE, SOURCE WHERE ');
        SQL.Add(' STATION.CRUISE_ID=CRUISE.ID AND ');
        SQL.Add(' CRUISE.SOURCE_ID=SOURCE.ID AND ');
-       SQL.Add(' SOURCE.NAME=:SrcName AND ');
+       SQL.Add(' SOURCE.PRIORITY=:R_LEV AND ');
        SQL.Add(' LATITUDE BETWEEN :LatMin AND :LatMax AND ');
        SQL.Add(' LONGITUDE BETWEEN :LonMin AND :LonMax AND ');
        SQL.Add(' DATEANDTIME BETWEEN :DateMin AND :DateMax AND ');
-       SQL.Add(' DUPLICATE=FALSE ');
-       ParamByName('SrcName').AsString:=cbSource.Text;
+       SQL.Add(' STATION.DUPLICATE=FALSE ');
+       ParamByName('R_LEV').Value:=r_lev+1; //One step lower
        ParambyName('LatMin').AsFloat:=Qt1.FieldByName('LATITUDE').AsFloat-seThreshold.Value;
        ParambyName('LatMax').AsFloat:=Qt1.FieldByName('LATITUDE').AsFloat+seThreshold.Value;
        ParambyName('LonMin').AsFloat:=Qt1.FieldByName('LONGITUDE').AsFloat-seThreshold.Value;
@@ -146,12 +137,12 @@ Qt2.Transaction:=TRt;
            SQL.Clear;
            SQL.Add(' UPDATE STATION SET ');
            SQL.Add(' DUPLICATE=TRUE WHERE');
-           SQL.Add(' ID=:ID ');
+           SQL.Add(' ID=:ID AND DUPLICATE=FALSE');
            ParamByName('ID').AsInteger:=Qt2.FieldByName('ID').AsInteger;
          ExecSQL;
         end;
         Qt2.Next;
-        caption:='Duplicates: '+inttostr(cnt_dup);
+        caption:='Step: '+inttostr(r_lev)+', Duplicates: '+inttostr(cnt_dup);
         Application.ProcessMessages;
       end;
 
@@ -162,6 +153,8 @@ Qt2.Transaction:=TRt;
     Qt1.Next;
    end;
 
+  end; //r_lev
+
 
  finally
   Showmessage('Found '+inttostr(cnt_dup)+' duplicates');
@@ -171,6 +164,9 @@ Qt2.Transaction:=TRt;
   {$ENDIF}
 
   btnFindDuplicates.Enabled:=true;
+  btnUpdateCruise.Enabled:=true;
+  btnMarkCruiseDuplicate.Enabled:=true;
+
   frmdm.TR.CommitRetaining;
   Trt.Commit;
   Qt1.Close;
@@ -184,16 +180,16 @@ procedure Tfrmqc_duplicates.btnUpdateCruiseClick(Sender: TObject);
 Var
  cnt_dup:integer;
 begin
+ try
+   btnFindDuplicates.Enabled:=false;
+   btnUpdateCruise.Enabled:=false;
+   btnMarkCruiseDuplicate.Enabled:=false;
 
     with frmdm.q1 do begin
       Close;
         SQL.Clear;
         SQL.Add(' SELECT ID from CRUISE ');
-        SQL.Add(' WHERE ');
-        SQL.Add(' SOURCE_ID IN (SELECT SOURCE.ID FROM SOURCE WHERE ');
-        SQL.Add(' SOURCE.NAME=:SrcName) ');
         SQL.Add(' ORDER BY CRUISE.ID ');
-        ParamByName('SrcName').AsString:=cbSource.Text;
       Open;
      end;
 
@@ -225,11 +221,20 @@ begin
 
   frmdm.q1.Next;
 end;
-    frmdm.TR.CommitRetaining;
-    showmessage('Update completed');
+
+  showmessage('Update completed');
+
+ finally
+  btnFindDuplicates.Enabled:=true;
+  btnUpdateCruise.Enabled:=true;
+  btnMarkCruiseDuplicate.Enabled:=true;
+
+  frmdm.TR.CommitRetaining;
+ end;
+
 end;
 
-procedure Tfrmqc_duplicates.Button1Click(Sender: TObject);
+procedure Tfrmqc_duplicates.btnMarkCruiseDuplicateClick(Sender: TObject);
 Var
  TRt:TSQLTransaction;
  Qt1, Qt2:TSQLQuery;
@@ -238,6 +243,12 @@ Var
  dat:text;
  st_db, st_dup, cnt_dup: integer;
 begin
+
+try
+
+btnFindDuplicates.Enabled:=false;
+btnUpdateCruise.Enabled:=false;
+btnMarkCruiseDuplicate.Enabled:=false;
 
 TRt:=TSQLTransaction.Create(self);
 TRt.DataBase:=frmdm.IBDB;
@@ -269,7 +280,7 @@ writeln(dat, 'Duplicated cruises');
       st_db:=Qt1.Fields[1].AsInteger;
       st_dup:=Qt1.Fields[2].AsInteger;
 
-      if st_dup/st_db>=seRatio.Value then begin
+      if st_dup/st_db>=seRatio.Value/100 then begin
       // memo1.Lines.Add(inttostr(id));
        with Qt2 do begin
          Close;
@@ -286,13 +297,22 @@ writeln(dat, 'Duplicated cruises');
       end;
      Qt1.Next;
     end;
+
+
+  showmessage('Duplicated cruises: '+inttostr(cnt_dup));
+  OpenDocument(GlobalUnloadPath+'dup_cruises.txt');
+finally
+
+  btnFindDuplicates.Enabled:=true;
+  btnUpdateCruise.Enabled:=true;
+  btnMarkCruiseDuplicate.Enabled:=true;
+
   Trt.Commit;
   Qt1.Free;
   Qt2.Free;
   Trt.Free;
   closeFile(dat);
-  showmessage('Duplicated cruises: '+inttostr(cnt_dup));
-  OpenDocument(GlobalUnloadPath+'dup_cruises.txt');
+end;
 end;
 
 end.
